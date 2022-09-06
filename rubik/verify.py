bbe5dfcaa9b420a70e5523ef662e2dd9ff2d9038
George Martin <gbm0016@auburn.edu>
2022-09-06 09:30:57 -0500

    Green Light |

--- rubik/verify.py
+++ rubik/verify.py
@@ -1,10 +1,8 @@
 import rubik.cube as rubik
 
-def _verify(parms):
-    """Determines if the provided cube is physically valid. Returns:
-       {'status': 'ok'} if valid 
-       {'status': 'error: xxx} if invalid"""
-    result = {}
-    encodedCube = parms.get('cube',None)       #STUB:  get "cube" parameter if present
-    result['status'] = 'ok'                     
-    return result
+def isValidLengthCube(cube):
+   if (len(cube) == 54):
+       return True
+   else:
+       return False
+   
