f57af735e319ad55561c3e7fb41c28c0c5feb332
George Martin <gbm0016@auburn.edu>
2022-09-15 11:52:55 -0500

    Red Light |

--- rubik/rotate.py
+++ rubik/rotate.py
@@ -5,6 +5,51 @@ def _rotate(parms):
     result = {}
     #encodedCube = parms.get('cube',None)       #STUB:  get "cube" parameter if present
     #rotatedCube = encodedCube                  #STUB:  rotate the cube
-    result['cube'] = 'gggggggggyooyooyoobbbbbbbbbrrwrrwrrwyyyyyyrrrooowwwwww'               
+    
+    #alt1
+    #result['cube'] = 'gggggggggyooyooyoobbbbbbbbbrrwrrwrrwyyyyyyrrrooowwwwww'   
+    
+    #alt2
+    # cube = parms.get('cube')
+    # cubeList = list(cube)
+    # rotatedCubeList = cubeList[:]
+    #
+    # #rotate front face
+    # rotatedCubeList[2] = cubeList[0]
+    # rotatedCubeList[5] = cubeList[1]
+    # rotatedCubeList[8] = cubeList[2]
+    # rotatedCubeList[1] = cubeList[3]
+    # rotatedCubeList[4] = cubeList[4]
+    # rotatedCubeList[7] = cubeList[5]
+    # rotatedCubeList[0] = cubeList[6]
+    # rotatedCubeList[3] = cubeList[7]
+    # rotatedCubeList[6] = cubeList[8]
+    #
+    # #rotate top to right
+    # rotatedCubeList[9] = cubeList[42]
+    # rotatedCubeList[12] = cubeList[43]
+    # rotatedCubeList[]15 = cubeList[44]
+    #
+    # #rotate top to right
+    # rotatedCubeList[47] = cubeList[9]
+    # rotatedCubeList[46] = cubeList[12]
+    # rotatedCubeList[45] = cubeList[15]
+    #
+    # #rotate bottom to left
+    # rotatedCubeList[29] = cubeList[45]
+    # rotatedCubeList[32] = cubeList[46]
+    # rotatedCubeList[35] = cubeList[47]
+    #
+    # #rotate left to top
+    # rotatedCubeList[44] = cubeList[29]
+    # rotatedCubeList[43] = cubeList[32]
+    # rotatedCubeList[42] = cubeList[36]
+    #
+    # rotate = "".join(rotatedCubeList)
+    
+    myCube = rubik.Cube(parms.get('cube'))
+    myCube.rotate(parma.get('dir'))
+    result['cube'] = myCube.toString()
+                
     result['status'] = 'ok'                     
     return result
---
+++ rubik/test/cubeTest.py
@@ -0,0 +1,34 @@
+'''
+Created on Sep 15, 2022
+
+@author: Martin
+'''
+import unittest
+import rubik.cube as cube
+
+# Analysis:    Cube:    class
+#    methods:    instantiate
+#                rotate
+#                toString
+#
+# Analysis:    Cube .__init__
+#    inputs:
+#        initialCube:    string; string; len=54, [brgoyw], 9 occurrences of each, unique middle, mandator, unvalidated
+#    outputs:
+#        side effects:    none
+#    nominal:
+#        Instance of Cube
+#    abnormal:
+#        exception
+#
+#
+#
+
+
+class Test(unittest.TestCase):
+
+
+    def test_init_010_shouldInsantiateCube(self):
+        incomingCube = 'gggggggggyooyooyoobbbbbbbbbrrwrrwrrwyyyyyyrrrooowwwwww'
+        myCube = cube.Cube(incomingCube)
+        self.assertIsInstance(myCube, cube.Cube)
