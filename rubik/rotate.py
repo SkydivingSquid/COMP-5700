c34012194cf228bb5c79fce3b88fbe07032b3f80
George Martin <gbm0016@auburn.edu>
2022-09-15 10:12:42 -0500

    Green Light |

--- rubik/rotate.py
+++ rubik/rotate.py
@@ -3,8 +3,8 @@ import rubik.cube as rubik
 def _rotate(parms):
     """Return rotated cube""" 
     result = {}
-    encodedCube = parms.get('cube',None)       #STUB:  get "cube" parameter if present
-    rotatedCube = encodedCube                  #STUB:  rotate the cube
-    result['cube'] = rotatedCube               
+    #encodedCube = parms.get('cube',None)       #STUB:  get "cube" parameter if present
+    #rotatedCube = encodedCube                  #STUB:  rotate the cube
+    result['cube'] = 'bygrbgbyowggwrrbbgybwbgbyrbgoroowowrryrgyrwooygwowwyyo'               
     result['status'] = 'ok'                     
     return result
