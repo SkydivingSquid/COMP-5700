0a4bd35f751b7c7d3311a84916930d7e2738eca9
George Martin <gbm0016@auburn.edu>
2022-09-16 18:43:18 -0500

    Red Light |

--- rubik/test/rotateTest.py
+++ rubik/test/rotateTest.py
@@ -28,12 +28,27 @@ class RotateTest(unittest.TestCase):
     #    confidence level:    boundary level analysis
     #    
     #    happy paths:
+    #        (General rotation instances)
     #        test 010: nominal valid cube with F rotation
     #        test 020: nominal valid cube with f rotation
-    #        etc, etc
-    #        test 030: nominal valid cube with missing rotation
-    #        test 040: valid cube with empty "" rotation
-    #        test 050: nominal valid cube with missing 'dir' key
+    #        test 030: nominal valid cube with R rotation
+    #        test 040: nominal valid cube with r rotation
+    #        test 050: nominal valid cube with B rotation
+    #        test 060: nominal valid cube with b rotation
+    #        test 070: nominal valid cube with L rotation
+    #        test 080: nominal valid cube with l rotation
+    #        test 090: nominal valid cube with U rotation
+    #        test 100: nominal valid cube with u rotation
+    #        test 110: nominal valid cube with D rotation
+    #        test 120: nominal valid cube with d rotation
+    #        test 130: nominal valid cube with multiple rotations.
+    #
+    #        (Unique rotation instances)
+    #        test 140: nominal valid cube with missing rotation
+    #        test 150: nominal valid cube with "" rotation
+    #        test 160: nominal valid cube with missing 'dir' key
+    #
+    #        test 199: nominal valid cube with multiple rotations.
     #
     #    sad paths:
     #        test 910: missing cube with valid rotation
@@ -53,7 +68,7 @@ class RotateTest(unittest.TestCase):
         self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
         self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
         
-    def test_rotate_011_ShouldRotateValidNominalCubef(self):
+    def test_rotate_020_ShouldRotateValidNominalCubef(self):
         inputDict = {}
         inputDict['op'] = 'rotate'
         inputDict['cube'] = 'gorwrgrobybgrbrorgoywbowrwrygobyybyboryyggbgwgbwowwwoy'
@@ -67,6 +82,19 @@ class RotateTest(unittest.TestCase):
         self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
         self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
         
+        def test_rotate_030_ShouldRotateROnNominalCube(self):
+        inputDict = {}
+        inputDict['op'] = 'rotate'
+        inputDict['cube'] = 'bbgwyrwybbboggrwwwrbrwwyryygooooborryoywbgwyyoggorgbrg'
+        inputDict['dir'] = 'R'
+        
+        expectedResult = {}
+        expectedResult['cube'] = 'bbgwygwygwgbwgbwroybrgwyyyygooooborryogwbrwybogrorwbrr'
+        expectedResult['status'] = 'ok'
+        
+        actualResult = rotate._rotate(inputDict)
+        self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
+        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
 
         
         
