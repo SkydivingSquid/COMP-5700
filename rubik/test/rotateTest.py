651009b8584eb0ae80ba37e29cc4b92a9ec0b97e
George Martin <gbm0016@auburn.edu>
2022-09-16 22:25:46 -0500

    Yellow Light |

--- rubik/test/rotateTest.py
+++ rubik/test/rotateTest.py
@@ -269,10 +269,10 @@ class RotateTest(unittest.TestCase):
         inputDict = {}
         inputDict['op'] = 'rotate'
         inputDict['cube'] = 'rgborogwrwbgbbrgrgoywbowrwrygwbygbyboryyggyrooybowwwoy'
-        inputDict['dir'] = "Ff"
+        inputDict['dir'] = "F"
     
         expectedResult = {}       
-        expectedResult['cube'] = 'rgborogwrwbgbbrgrgoywbowrwrygwbygbyboryyggyrooybowwwoy'
+        expectedResult['cube'] = 'gorwrgrobybgrbrorgoywbowrwrygobyybyboryyggbgwgbwowwwoy'
         expectedResult['status'] = 'ok'
     
         actualResult = rotate._rotate(inputDict)
