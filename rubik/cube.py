33b55f61d4ebfb6853e383f75be65f7bf1a1d674
George Martin <gbm0016@auburn.edu>
2022-09-17 17:52:20 -0500

    Green Light |

--- rubik/cube.py
+++ rubik/cube.py
@@ -36,7 +36,7 @@ class Cube:
         
     def isValidDirChar(dir):
         
-        allowed_dirChar = "frblud"
+        allowed_dirChar = "frbludFRBLUD"
 
         if all(ch in allowed_dirChar for ch in dir):
             return True
