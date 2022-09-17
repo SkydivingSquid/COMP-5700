5ae3beb307b90b367df3bf7b2880253a17a77194
George Martin <gbm0016@auburn.edu>
2022-09-17 13:15:07 -0500

    Red Light |

--- rubik/test/rotateTest.py
+++ rubik/test/rotateTest.py
@@ -269,7 +269,7 @@ class RotateTest(unittest.TestCase):
         inputDict = {}
         inputDict['op'] = 'rotate'
         inputDict['cube'] = 'wwwwwwwwwrrrrrrrrryyyyyyyyyooooooooobbbbbbbbbggggggggg'
-        inputDict['dir'] = "F"
+        inputDict['dir'] = "FF"
     
         expectedResult = {}       
         expectedResult['cube'] = 'wwwwwwwwwrrrrrrrrryyyyyyyyyooooooooobbbbbbbbbggggggggg'
