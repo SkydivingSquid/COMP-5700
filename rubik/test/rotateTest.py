8344cd49195fb593a7e8f5844785af48ccb585f8
George Martin <gbm0016@auburn.edu>
2022-09-17 18:35:27 -0500

    Green Light |

--- rubik/test/rotateTest.py
+++ rubik/test/rotateTest.py
@@ -289,16 +289,16 @@ class RotateTest(unittest.TestCase):
     #     self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
     
     def test_rotate_540_Controller_ValidDirChars(self):
-    inputDict = {}
-    inputDict['op'] = 'rotate'
-    inputDict['cube'] = 'rgborogwrwbgbbrgrgoywbowrwrygwbygbyboryyggyrooybowwwoy'
-    inputDict['dir'] = 'a'
-
-    expectedResult = {}
-    expectedResult['status'] = 'Error - Invalid Dir Char'
-
-    actualResult = rotate._controller(inputDict)
-    self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
+        inputDict = {}
+        inputDict['op'] = 'rotate'
+        inputDict['cube'] = 'rgborogwrwbgbbrgrgoywbowrwrygwbygbyboryyggyrooybowwwoy'
+        inputDict['dir'] = 'a'
+        
+        expectedResult = {}
+        expectedResult['status'] = 'Error - Invalid Dir Char'
+        
+        actualResult = rotate._controller(inputDict)
+        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
         
         
     #
