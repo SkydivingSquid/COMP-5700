89a83ae0b463a8d01e0e798920182d85c462f199
George Martin <gbm0016@auburn.edu>
2022-09-17 15:41:13 -0500

    Yellow Light |

--- rubik/rotate.py
+++ rubik/rotate.py
@@ -7,66 +7,76 @@ def _rotate(parms):
     direc = parms.get('dir', 'F')
     #direction = parms.get('dir', 'F')
     #direc = list(direction)
-    
-    
-    if direc == "" or direc == None:
-    #if direc == "":
-        direc = 'F'
-    
-    
-    for x in direc:
-    
-        if x == 'F':
-            parms = _rotateF(cube, dir)
-            cube = parms.get('cube')
-            
-        elif x == 'f':
-            parms = _rotatef(cube, dir)
-            cube = parms.get('cube')
-            
-        elif x == 'R':
-            parms = _rotateR(cube, dir)
-            cube = parms.get('cube')
-            
-        elif x == 'r':
-            parms = _rotater(cube, dir)
-            cube = parms.get('cube')
-            
-        elif x == 'B':
-            parms = _rotateB(cube, dir)
-            cube = parms.get('cube')
-            
-        elif x == 'b':
-            parms = _rotateb(cube, dir)
-            cube = parms.get('cube')
-            
-        elif x == 'L':
-            parms = _rotateL(cube, dir)
-            cube = parms.get('cube')
-            
-        elif x == 'l':
-            parms = _rotatel(cube, dir)
-            cube = parms.get('cube')
-            
-        elif x == 'U':
-            parms = _rotateU(cube, dir)
-            cube = parms.get('cube')
-            
-        elif x == 'u':
-            parms = _rotateu(cube, dir)
-            cube = parms.get('cube')
-    
-        elif x == 'D':
-            parms = _rotateD(cube, dir)
-            cube = parms.get('cube')
-            
-        elif x == 'd':
-            parms = _rotated(cube, dir)
-            cube = parms.get('cube')
-             
-        result['cube'] = cube
-      
     result['status'] = 'ok'
+    
+    while result['status'] == 'ok':
+        
+        if direc == "" or direc == None:
+        #if direc == "":
+            direc = 'F'
+            
+        if rubik.Cube.isValidLengthCube(cube) == False:
+            
+        
+            
+            
+            
+            
+            
+        
+        
+        for x in direc:
+        
+            if x == 'F':
+                parms = _rotateF(cube, dir)
+                cube = parms.get('cube')
+                
+            elif x == 'f':
+                parms = _rotatef(cube, dir)
+                cube = parms.get('cube')
+                
+            elif x == 'R':
+                parms = _rotateR(cube, dir)
+                cube = parms.get('cube')
+                
+            elif x == 'r':
+                parms = _rotater(cube, dir)
+                cube = parms.get('cube')
+                
+            elif x == 'B':
+                parms = _rotateB(cube, dir)
+                cube = parms.get('cube')
+                
+            elif x == 'b':
+                parms = _rotateb(cube, dir)
+                cube = parms.get('cube')
+                
+            elif x == 'L':
+                parms = _rotateL(cube, dir)
+                cube = parms.get('cube')
+                
+            elif x == 'l':
+                parms = _rotatel(cube, dir)
+                cube = parms.get('cube')
+                
+            elif x == 'U':
+                parms = _rotateU(cube, dir)
+                cube = parms.get('cube')
+                
+            elif x == 'u':
+                parms = _rotateu(cube, dir)
+                cube = parms.get('cube')
+        
+            elif x == 'D':
+                parms = _rotateD(cube, dir)
+                cube = parms.get('cube')
+                
+            elif x == 'd':
+                parms = _rotated(cube, dir)
+                cube = parms.get('cube')
+                 
+            result['cube'] = cube
+      
     
     return result
             
