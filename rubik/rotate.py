8d99e9f618f916f10ea2b45679bdd64b4e483edc
George Martin <gbm0016@auburn.edu>
2022-09-17 20:07:09 -0500

    Green Light |

--- rubik/rotate.py
+++ rubik/rotate.py
@@ -26,7 +26,7 @@ def _controller(parms):
     #And it made me feel more confident in my knowledge
     #Of Python.. 
     
-    if rubik.Cube.doesCubeExist(cube) == False
+    if rubik.Cube.doesCubeExist(cube) == False:
         result['status'] = 'Error - Missing Cube'
         status = result['status']
     
