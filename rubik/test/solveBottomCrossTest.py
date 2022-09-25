6c96a613818eabed3b0885c4571342a5a2b1a45e
George Martin <gbm0016@auburn.edu>
2022-09-25 12:34:46 -0500

    Green Light |

--- rubik/test/solveBottomCrossTest.py
+++ rubik/test/solveBottomCrossTest.py
@@ -52,19 +52,31 @@ class solveBottomCrossTest(unittest.TestCase):
     #     self.assertEqual(expectedResult.get('solution'), actualResult.get('solution'))
     #     self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
         
-    def test_solve_021_ShouldAlignDaisyAndSolveForBottomCross(self):
+    # def test_solve_021_ShouldAlignDaisyAndSolveForBottomCross(self):
+    #     inputDict = {}
+    #     inputDict['op'] = 'solve'
+    #     inputDict['cube'] = 'wybgbbrowwrryryrgbgogogrobywbogoyoryogyoybbrrbwgwwwgwy'
+    #
+    #     expectedResult = {}       
+    #     expectedResult['cube'] = 'gyrobgbbyyroyrgorwwoorgbgowbgryoyrbwywbwywyrbrwgowbggo'
+    #     expectedResult['solution'] = 'FFRRBBLLUUFF'
+    #     expectedResult['status'] = 'ok'
+    #
+    #     actualResult = solve._solve(inputDict)
+    #     self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
+    #     self.assertEqual(expectedResult.get('solution'), actualResult.get('solution'))
+    #     self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
+    
+        def test_solve_999_PlayGroundTest(self):
         inputDict = {}
-        inputDict['op'] = 'solve'
         inputDict['cube'] = 'wybgbbrowwrryryrgbgogogrobywbogoyoryogyoybbrrbwgwwwgwy'
     
         expectedResult = {}       
         expectedResult['cube'] = 'gyrobgbbyyroyrgorwwoorgbgowbgryoyrbwywbwywyrbrwgowbggo'
-        expectedResult['solution'] = 'FFRRBBLLUUFF'
-        expectedResult['status'] = 'ok'
-    
-        actualResult = solve._solve(inputDict)
+        expectedResult['daisySolution'] = 'FFRRBBLLUUFF'
+
+        actualResult = solve._daisySolution(inputDict)
         self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
-        self.assertEqual(expectedResult.get('solution'), actualResult.get('solution'))
-        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
-        
+        self.assertEqual(expectedResult.get('daisySolution'), actualResult.get('daisySolution'))
+   
         
