5e2d5f0e407b601d970f71bfde8548d236c1bca1
George Martin <gbm0016@auburn.edu>
2022-09-26 10:28:52 -0500

    Green Light |

--- rubik/test/solveBottomCrossTest.py
+++ rubik/test/solveBottomCrossTest.py
@@ -429,8 +429,8 @@ class solveBottomCrossTest(unittest.TestCase):
         inputDict['cube'] = 'yogrwbbwowwoyrorbwryobboyrybbwggwbwrggbgogrroyywoyrgyg'
     
         expectedResult = {}       
-        expectedResult['cube'] = ''
-        expectedResult['solution'] = ''
+        expectedResult['cube'] = 'ywoowbrwgrorgrobrgyrobbwwbrgrbggrygowwbboggobwyoyyywyy'
+        expectedResult['solution'] = 'FFBBUFBBrbRDBBUUUFFURRUUBBULL'
         expectedResult['status'] = 'ok'
     
         actualResult = solve._solve(inputDict)
