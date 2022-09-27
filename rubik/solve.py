5aff324f19de3acc8de2492737d1aded90afeef5
George Martin <gbm0016@auburn.edu>
2022-09-27 09:52:15 -0500

    Refactor |

--- rubik/solve.py
+++ rubik/solve.py
@@ -229,28 +229,13 @@ def _solveBottomCross(encodedCube):
             #Checking Right of Bottom Face
             if(numberOfPetalsFound <= 3):
                 if rotatedCubeList[50] == rotatedCubeList[49]:
-                    while rotatedCubeList[50] == rotatedCubeList[41]:
-                        U_result = _rotateU(encodedCube) 
-                        result['solution'] += U_result.get('letter')
-                        encodedCube = U_result.get('cube')
-                        
-                        rotatedCubeList = encodedCube
                     
-                    if rotatedCubeList[50] != rotatedCubeList[41]:
+                    bottomToDaisyResult = _unalignedBottomToDaisy(50, 41, result['solution'], rotatedCubeList)
+                    result['solution'] = bottomToDaisyResult.get('solution')
+                    rotatedCubeList = bottomToDaisyResult.get('rotatedCubeList')
                         
-                        R_result = _rotateR(encodedCube)
-                        result['solution'] += R_result.get('letter')
-                        encodedCube = R_result.get('cube')
-                    
-                        R_result = _rotateR(encodedCube)
-                        result['solution'] += R_result.get('letter')
-                        encodedCube = R_result.get('cube')
-                        
-                        result['cube'] = encodedCube
-
-                        rotatedCubeList = encodedCube
-                        
-                        numberOfPetalsFound += 1
+                    rotatedCubeList = encodedCube
+                    numberOfPetalsFound += 1
 
                     
             #Checking Bottom of Bottom Face
