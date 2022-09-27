96c687780c590057dc950e8614a1846a11e56045
George Martin <gbm0016@auburn.edu>
2022-09-27 18:44:54 -0500

    Refactor |

--- rubik/solve.py
+++ rubik/solve.py
@@ -606,7 +606,7 @@ def _verticalCubesToDaisy(verticalPetalIndex: int, topPetalIndex: int, solution,
         
         #Move l,f,L,D,F,F
         if verticalPetalIndex == 1:
-            veritcalToDaisyResult['solution'], rotatedCubeList = _verticleUpperCubeIntoDaisy_Front(rotatedCubeList, veritcalToDaisyResult['solution'])
+            #veritcalToDaisyResult['solution'], rotatedCubeList = _verticleUpperCubeIntoDaisy_Front(rotatedCubeList, veritcalToDaisyResult['solution'])
         
         
         if verticalPetalIndex == 7:
