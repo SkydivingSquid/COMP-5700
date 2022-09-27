18e3d26e3c8c060745e102e5b318b76e2951003f
George Martin <gbm0016@auburn.edu>
2022-09-26 23:32:58 -0500

    Green Light |

--- rubik/test/solveBottomCrossTest.py
+++ rubik/test/solveBottomCrossTest.py
@@ -502,6 +502,33 @@ class solveBottomCrossTest(unittest.TestCase):
         self.assertEqual(expectedResult.get('list'), actualResult.get('daisyCubeList'))
         self.assertEqual(expectedResult.get('solution'), actualResult.get('solution'))
 
+        
+    def test_solve_1001_RefactoredDaisyRot(self):
+        inputList = ['w', 'o', 'b', 'b', 'o', 'r', 'o', 'y', 'y', 
+                     'r', 'y', 'g', 'o', 'y', 'o', 'o', 'b', 'g', 
+                     'b', 'g', 'g', 'y', 'g', 'b', 'w', 'g', 'o', 
+                     'r', 'r', 'g', 'r', 'r', 'o', 'y', 'y', 'r', 
+                     'b', 'w', 'y', 'w', 'b', 'w', 'r', 'w', 'o', 
+                     'w', 'b', 'b', 'g', 'w', 'g', 'w', 'r', 'y']
+        
+        solution = ""
+        
+        expectedResult = {}
+        expectedResult['list'] = ['y', 'y', 'o', 'r', 'o', 'b', 'b', 'o', 'w', 
+                                  'r', 'y', 'g', 'o', 'y', 'o', 'g', 'b', 'g', 
+                                  'b', 'g', 'g', 'y', 'g', 'b', 'w', 'g', 'o', 
+                                  'r', 'r', 'o', 'r', 'r', 'o', 'y', 'y', 'r', 
+                                  'b', 'w', 'y', 'w', 'b', 'w', 'b', 'b', 'w', 
+                                  'o', 'w', 'r', 'g', 'w', 'g', 'w', 'r', 'y']
+                                 
+        expectedResult['solution'] = 'FF'
+        
+        
+        actualResult = solve._daisy_Rotations(4, 1, inputList, solution)
+        
+        self.assertEqual(expectedResult.get('list'), actualResult.get('daisyCubeList'))
+        self.assertEqual(expectedResult.get('solution'), actualResult.get('solution'))
+
 
 
 
