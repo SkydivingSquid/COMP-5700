5a2226150557ec3438f578a6ce18d7966db29982
George Martin <gbm0016@auburn.edu>
2022-10-07 12:59:29 -0500

    Refactor |

--- rubik/solve.py
+++ rubik/solve.py
@@ -320,7 +320,7 @@ def _daisy_Rotations(uniqueCenter: int, topMiddle: int, encodedCube, solution):
     
     if rotatedCubeList[uniqueCenter] == rotatedCubeList[topMiddle]:
         if uniqueCenter == 4:
-            encodedCube = _functionDoubleF_Daisy(encodedCube, daisyRotResult)
+            encodedCube = _verticalCubeIntoDaisy(encodedCube, daisyRotResult, 'FF')
             
         if uniqueCenter == 13:
             encodedCube = _funtionDoubleR_Daisy(encodedCube, daisyRotResult)
