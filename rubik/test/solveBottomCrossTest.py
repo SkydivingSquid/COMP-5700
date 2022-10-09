a68bcc2779e9f79cf9dc74b68cc5751c136bcfd6
George Martin <gbm0016@auburn.edu>
2022-10-09 12:01:17 -0500

    Refactor |

--- rubik/test/solveBottomCrossTest.py
+++ rubik/test/solveBottomCrossTest.py
@@ -504,12 +504,12 @@ class solveBottomCrossTest(unittest.TestCase):
         #Needs specific type of cube, and specific type of solution (ie result)
     def test_999_functionalMovement(self):
         
-        cube = 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'
+        cube = ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
         solution = {}
         solution['solution'] = '' 
         letter = 'f'
         
-        expectedResult = 'f', 'ybbbbobbywrryrrrrrggoggggggbboooooooyyyyyygrrwwbwwwwww'
+        expectedResult = 'f', ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'w', 'r', 'r', 'w', 'r', 'r', 'w', 'r', 'r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'o', 'o', 'y', 'o', 'o', 'y', 'o', 'o', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'r', 'r', 'r', 'o', 'o', 'o', 'w', 'w', 'w', 'w', 'w', 'w']
         actualResult = solve._functionalRotations(cube, solution, letter)
         self.assertEqual(expectedResult, actualResult)
                                              
