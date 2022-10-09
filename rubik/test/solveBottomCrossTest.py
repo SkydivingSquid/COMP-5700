5c2ab35bc703074fd1e630c9c929284fa2facd4a
George Martin <gbm0016@auburn.edu>
2022-10-09 12:18:00 -0500

    Green Light |

--- rubik/test/solveBottomCrossTest.py
+++ rubik/test/solveBottomCrossTest.py
@@ -501,6 +501,15 @@ class solveBottomCrossTest(unittest.TestCase):
         actualResult = solve._moveBottomEdgeToTopEdge(cube, solution, value)
         self.assertEqual(expectedResult,actualResult)
         
+    def test_solve_034a_movingBottomEdgeToTopEdge2(self):
+        cube = 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'
+        solution = {}
+        solution['solution'] = '' 
+        value = 8
+    
+        expectedResult = ['y', 'b', 'b', 'b', 'b', 'o', 'b', 'b', 'y', 'w', 'r', 'r', 'y', 'r', 'r', 'r', 'r', 'r', 'g', 'g', 'o', 'g', 'g', 'g', 'g', 'g', 'g', 'b', 'b', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'y', 'y', 'y', 'y', 'y', 'y', 'g', 'r', 'r', 'w', 'w', 'b', 'w', 'w', 'w', 'w', 'w', 'w'], 'fuFU',1
+        actualResult = solve._moveBottomEdgeToTopEdge(cube, solution, value)
+        self.assertEqual(expectedResult,actualResult)
         
         
         #Needs specific type of cube, and specific type of solution (ie result)
