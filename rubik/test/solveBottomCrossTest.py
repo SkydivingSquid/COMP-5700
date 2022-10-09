06331508df21bcf031e4fc289061c863cf3ed1e2
George Martin <gbm0016@auburn.edu>
2022-10-09 11:54:24 -0500

    Yellow Light |

--- rubik/test/solveBottomCrossTest.py
+++ rubik/test/solveBottomCrossTest.py
@@ -505,7 +505,7 @@ class solveBottomCrossTest(unittest.TestCase):
         
         cube = ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
         solution = {}
-        solution['solution'] == '' 
+        solution['solution'] = '' 
         letter = 'f'
         
         expectedResult = 'bbbbbbbbbwrrwrrwrrgggggggggooyooyooyyyyyyyrrrooowwwwww', 'f'
