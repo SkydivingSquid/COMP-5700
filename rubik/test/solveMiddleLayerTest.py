1ebced886f372a9740f167b305329134a7af2ee2
George Martin <gbm0016@auburn.edu>
2022-10-20 21:05:39 -0500

    Refactor |

--- rubik/test/solveMiddleLayerTest.py
+++ rubik/test/solveMiddleLayerTest.py
@@ -8,23 +8,6 @@ import rubik.solve as solve
 
 
 class solveMiddleLayerTest(unittest.TestCase):
-    
-
-    # def test_solve_040_SolvingTheMiddleLayer_ComplexScramble(self):
-    #     inputDict = {}
-    #     inputDict['op'] = 'solve'
-    #     inputDict['cube'] = 'ybwrbgoyyowgwrybwroowogggwbrggyobygwbbyryrrygbroowbrow' #Scrambled
-    #
-    #     expectedResult = {}
-    #     expectedResult['cube'] = list('gbybbbbbbgryrrrrrrbyyggggggoyrooooooborgyyyyowwwwwwwww')
-    #     expectedResult['solution'] = 'ffrFDRRLLblBDLLUUfFFUURRUUBBUULLbuBUUUufUFruRUruRUubuuBUbuBUluLUluLUURurufUFUUURurufUFUUuurURUBubuFufulULuuubUBULulUFufulUL' 
-    #     expectedResult['status'] = 'ok'
-    #
-    #     actualResult = solve._solve(inputDict)
-    #     self.assertEqual(expectedResult.get('solution'), actualResult.get('rotations'))
-    #     self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
-    #
-
         
     def test_solve_040_FindCorrectMiddleEdge(self):
         cube = 'rybobbbbbyygrrrrrryboggbgggyybyogooogoroyrygowwwwwwwww'
@@ -62,12 +45,43 @@ class solveMiddleLayerTest(unittest.TestCase):
         actualResult= solve._topToMiddleEdgeAlgorithm(cube, solution, cubeLocation, colorMarker)
         self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
         self.assertEqual(expectedResult.get('solution'), actualResult.get('solution'))
-        
-        
-        
-        
-        
-        
-        
+    
+    def test_solve_043_unalighedMiddleEdge(self):
+        cube = 'oyyobrbbbggbbryrrrrbyggbgggryyyogooogoyryrboowwwwwwwww'
+        solution = '' 
+        result = {}
+        cubeLctn =  5
+        initialEdge = 1
+        markerEdge1 = 43
+        markerEdge2 = 1 
+        center = 4
+        sideEdge1 = 13
+        sideEdge2 = 2
+    
+        expectedResult = {}
+        expectedResult['cube'] = list('bbbobbbbbyyyrryrrrgogggbgggygyyogoooryoryrroowwwwwwwww')
+        expectedResult['solution'] = 'URurufUFUUURurufUF'
+    
+        actualResult = {}
+        actualResult['cubeLctn'], actualResult['cube'], actualResult['solution'] = solve._unalighedMiddleEdge(cube, solution, result, cubeLctn, initialEdge, markerEdge1, markerEdge2, center, sideEdge1, sideEdge2)
+    
+        self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
+        self.assertEqual(expectedResult.get('solution'), actualResult.get('solution'))
+    
+    #
+    # def test_solve_045_SolvingTheMiddleLayer_ComplexScramble(self):
+    #     inputDict = {}
+    #     inputDict['op'] = 'solve'
+    #     inputDict['cube'] = 'ybwrbgoyyowgwrybwroowogggwbrggyobygwbbyryrrygbroowbrow' #Scrambled
+    #
+    #     expectedResult = {}
+    #     expectedResult['cube'] = list('gbybbbbbbgryrrrrrrbyyggggggoyrooooooborgyyyyowwwwwwwww')
+    #     expectedResult['solution'] = 'ffrFDRRLLblBDLLUUfFFUURRUUBBUULLbuBUUUufUFruRUruRUubuuBUbuBUluLUluLUURurufUFUUURurufUFUUuurURUBubuFufulULuuubUBULulUFufulUL' 
+    #     expectedResult['status'] = 'ok'
+    #
+    #     actualResult = solve._solve(inputDict)
+    #     self.assertEqual(expectedResult.get('solution'), actualResult.get('rotations'))
+    #     self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
+    
         
         
