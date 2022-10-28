c63b3c8205cdca219ef9958b74cf4a3a50f60bd9
George Martin <gbm0016@auburn.edu>
2022-10-28 11:27:07 -0500

    Green Light |

--- rubik/test/solveTopSurfaceTest.py
+++ rubik/test/solveTopSurfaceTest.py
@@ -22,22 +22,22 @@ class solveTopSurfaceTest(unittest.TestCase):
 
     
     
-    # def test_solve_051a_SolvingTheFace_ComplexScramble(self): #Integrated Test
-    #     inputDict = {}
-    #     inputDict['op'] = 'solve'
-    #     inputDict['cube'] = 'wgbgbrooboyobrgowygygogbrbrybroowwyyrowwyrgrwgwygwrbyb'
-    #
-    #     expectedResult = {}
-    #     expectedResult['cube'] = list('orbbbbbbbrbrrrrrrrggoggggggbogooooooyyyyyyyyywwwwwwwww')
-    #
-    #     #Optimized Version 
-    #     expectedResult['solution'] = 'FFUfRRfrFDRRUUFFUURRUUBBUULuLfuuFUfuFrURluLUbUBUlULuRurufUFrURUBubbUBULulFufulULFRUrufUUFRUruRUrufUURUrURUUrUURUrURUUr'
-    #     expectedResult['status'] = 'ok'
-    #
-    #     actualResult = solve._solve(inputDict)
-    #     self.assertEqual(expectedResult.get('solution'), actualResult.get('rotations'))
-    #     self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
-    #
+    def test_solve_051a_SolvingTheFace_ComplexScramble(self): #Integrated Test
+        inputDict = {}
+        inputDict['op'] = 'solve'
+        inputDict['cube'] = 'wgbgbrooboyobrgowygygogbrbrybroowwyyrowwyrgrwgwygwrbyb'
+    
+        expectedResult = {}
+        expectedResult['cube'] = list('orbbbbbbbrbrrrrrrrggoggggggbogooooooyyyyyyyyywwwwwwwww')
+    
+        #Optimized Version 
+        expectedResult['solution'] = 'FFUfRRfrFDRRUUFFUURRUUBBUULuLfuuFUfuFrURluLUbUBUlULuRurufUFrURUBubbUBULulFufulULFRUrufUUFRUruRUrufUURUrURUUrUURUrURUUr'
+        expectedResult['status'] = 'ok'
+    
+        actualResult = solve._solve(inputDict)
+        self.assertEqual(expectedResult.get('solution'), actualResult.get('rotations'))
+        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))
+    
 
 
         
