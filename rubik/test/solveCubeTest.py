ea28d6000fddfc9add33b2caabb758193f907f6a
George Martin <gbm0016@auburn.edu>
2022-11-13 19:04:40 -0600

    Red Light |

--- rubik/test/solveCubeTest.py
+++ rubik/test/solveCubeTest.py
@@ -10,3 +10,20 @@ import rubik.solve as solve
 class solveCubeTest(unittest.TestCase):
 
 
+    def test_solve_060a_countingHornsforTopCorners(self): #Integrated Test
+        cube = ''
+        flag = ''
+        hornColor = ''
+        hornLocation = ''
+                
+        expectedResult = {}
+        expectedResult['flag'] = 'none'
+        expectedResult['hornColor'] = 'None'
+        expectedResult['hornLocation'] = 'None'
+        
+        flag, hornColor, hornLocation, = solve._countHorns(cube)
+        self.assertEqual(expectedResult.get('flag'), flag)
+        self.assertEqual(expectedResult.get('hornColor'), hornColor)
+        self.assertEqual(expectedResult.get('hornLocation'), hornLocation)
+        
+        
